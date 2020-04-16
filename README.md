5fde6298fb513e3550589c5ac42ab80eabf61768
Justin Lettenmair <justin.lettenmair@gmail.com>
2020-04-15 21:53:22 -0500

    Change logic/copy to Gravity Forms

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-# Sage 9 and Sage 10 friendly Advanced Custom Fields configuration
+# Sage 9 and Sage 10 friendly Gravity Forms VCS form saving
 
-Sets up saving and loading of Advanced Custom Fields settings using [ACF Local JSON](https://www.advancedcustomfields.com/resources/local-json/).
+Sets up saving and loading of Gravity Forms forms
 
-Local `.json` files are stored in `your-theme-name/resources/assets/acf-json`
+Local `.json` files are stored in `your-theme-name/resources/assets/gravity-forms-json`
 
 To install, run the following in your Sage 9- and Sage 10-based theme directory:
 ```bash
-composer require "mwdelaney/sage-advanced-custom-fields"
+composer require "sermodigital/sage-gravity-forms"
 ```
+
+## Thanks
+
+This idea and implementation comes from [https://github.com/MWDelaney/sage-advanced-custom-fields](https://github.com/MWDelaney/sage-advanced-custom-fields)
--- gravity-forms.php
+++ gravity-forms.php
@@ -1,57 +1,52 @@
 <?php
 /**
- * Advanced Custom Fields drop-in functionality for Sage 9
+ * Gravity Forms saving for Sage 9 & 10
  * Version: 1.0
- * Author: Michael W. Delaney
+ * Author: Justin Lettenmair
  */
 
-// Hide ACF menu item in Production
-if (defined('WP_ENV') && WP_ENV == 'production') {
-    add_filter('acf/settings/show_admin', '__return_false');
-}
 
-if (function_exists('add_filter')) {
+if (function_exists('add_action')) {
 
-  /**
-   * Set local json save path
-   * @param  string $path unmodified local path for acf-json
-   * @return string       our modified local path for acf-json
-   */
-    add_filter('acf/settings/save_json', function ($path) {
-
-    // Set Sage9 friendly path at /theme-directory/resources/assets/acf-json
-
-        if (is_dir(get_stylesheet_directory() . '/assets')) {
+    add_action('gform_after_save_form', function($form, $is_new) {
+        // Set Sage9 friendly path at /theme-directory/resources/assets/gravity-forms-json
+        if(is_dir(get_stylesheet_directory() . '/assets')) {
             // This is Sage 9
-            $path = get_stylesheet_directory() . '/assets/acf-json';
+            $path = get_stylesheet_directory() . '/assets/gravity-forms-json';
         } else {
             // This is Sage 10
-            $path = get_stylesheet_directory() . '/resources/assets/acf-json';
+            $path = get_stylesheet_directory() . '/resources/assets/gravity-forms-json';
         }
 
-        // If the directory doesn't exist, create it.
-        if (!is_dir($path)) {
-            mkdir($path);
+        // make dir if does not exist
+        if(!file_exists($path)) {
+            mkdir($path, 0777, true);
         }
 
-        // Always return
-        return $path;
-    });
+        // bail early if dir isn't writable
+        if(!is_writable($path)) {
+            return false;
+        }
+
+        // Now that we have our path, let's save this form's JSON
+        $form_json = json_encode( $form );
+        $form_id   = $form['id'];
+        $file      = 'form_' . $form_id . '.json';
+
+        // write file
+        $f = fopen("{$path}/{$file}", 'w');
+        fwrite($f, $form_json);
+        fclose($f);
+
+        return true;
+    }, 10, 2 );
 
 
     /**
-     * Set local json load path
-     * @param  string $path unmodified local path for acf-json
-     * @return string       our modified local path for acf-json
+     * How should we handle loading these? We can use the logic from
+     * gravityformscli:
+     * https://github.com/gravityforms/gravityformscli/blob/master/includes/class-gf-cli-form.php#L241
+     * but we need a way to initiate it. Maybe a custom tab here:
+     * https://docs.gravityforms.com/gform_export_page_view/
      */
-    add_filter('acf/settings/load_json', function ($paths) {
-        // Sage 9 path
-        $paths[] = get_stylesheet_directory() . '/assets/acf-json';
-
-        // Sage 10 path
-        $paths[] = get_stylesheet_directory() . '/resources/assets/acf-json';
-
-        // return
-        return $paths;
-    });
 }
